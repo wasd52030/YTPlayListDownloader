024e37b79ad7f54067b8571f63578caf927f7caa
sobol <nppstcc1001050311@gmail.com>
2024-03-11 02:09:29 +0800

    把name換成title

--- JsonModel.cs
+++ JsonModel.cs
@@ -1,5 +1,4 @@
 using System.Text.Json.Serialization;
-using AngleSharp.Common;
 
 class Videos
 {
@@ -15,22 +14,22 @@ class Video
     [JsonPropertyName("id")]
     public string id { get; set; }
 
-    [JsonPropertyName("name")]
-    public string name { get; set; }
+    [JsonPropertyName("title")]
+    public string title { get; set; }
 
 
     [JsonPropertyName("comment")]
     public string? comment { get; set; }
 
-    public Video(string id, string name, string? comment)
+    public Video(string id, string title, string? comment)
     {
         this.id = id;
-        this.name = name;
+        this.title = title;
         this.comment = comment;
     }
 
     public override string ToString()
     {
-        return $"{GetType().Name}(id={id}, name={name}, description={comment})";
+        return $"{GetType().Name}(id={id}, name={title}, description={comment})";
     }
 }
