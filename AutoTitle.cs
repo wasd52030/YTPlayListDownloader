21ab5519a8d1459a4312d05e9930c1735abcbe93
sobel <nppstcc1001050311@gmail.com>
2024-06-26 02:01:28 +0800

    update json format

--- AutoTitle.cs
+++ AutoTitle.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Unicode;
@@ -7,21 +8,44 @@ class AutoTitle : Collector
 {
     public AutoTitle(string url) : base(url) { }
 
-    private async Task<List<Video>> Update(List<Video> videos)
+    private async Task<IEnumerable<Video>> Update(HashSet<Video> videos)
     {
         var playList = await yt.Playlists.GetVideosAsync(url).ToListAsync();
         var newVideos = playList.Where(playList => !videos.Any(video => video.Id == playList.Id))
                                 .Select(video => new Video(video.Id, video.Title, ""))
-                                .Concat(videos)
-                                .ToList();
-                                
-        Console.WriteLine($"origin = {videos.Count()}, updated = {newVideos.Count()}");
+                                .Concat(videos);
+
+        Console.WriteLine($"origin = {videos.Count}, updated = {newVideos.Count()}");
         return newVideos;
+    }
+
+    private async Task<HashSet<Video>> UpdatePlayListInfo(IEnumerable<Video> videos)
+    {
+
+        var playList = await yt.Playlists.GetVideosAsync(url).ToListAsync();
+        var PlaylistInfo = await yt.Playlists.GetAsync(url);
+        return videos.Select(v =>
+        {
+            var InPlaylist = playList.FirstOrDefault((playList) => playList.Id == v.Id);
+            if (InPlaylist != null)
+            {
+                var id = PlaylistInfo.Id;
+                var owner = PlaylistInfo.Author?.ChannelTitle!.Replace("by", "").Trim();
+                var title = PlaylistInfo.Title;
+                var track = playList.IndexOf(InPlaylist) + 1;
+                v.Playlists.Add(new PlaylistInfo(id, owner, title, track));
+            }
+            return v;
+        })
+        .ToHashSet();
     }
 
     public override async Task Invoke()
     {
         Console.WriteLine("updating...");
+
+        Stopwatch watch = new Stopwatch();
+        watch.Start();
 
         string jsonFile = await File.ReadAllTextAsync(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "./customTitle.json"));
         var jsonContent = JsonSerializer.Deserialize<Videos>(
@@ -29,14 +53,11 @@ class AutoTitle : Collector
             new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
         );
 
-        jsonContent.items = await Update(jsonContent.items);
+        jsonContent!.items = await UpdatePlayListInfo(await Update(jsonContent.items));
 
         if (jsonContent != null)
         {
-            jsonContent.items.Sort((Video a, Video b) =>
-            {
-                return a.Title.CompareTo(b.Title);
-            });
+            jsonContent.items = jsonContent.items.ToList().OrderBy(v => v.Title).ToHashSet();
 
             var finalJson = JsonSerializer.Serialize<Videos>(
                 jsonContent,
@@ -46,6 +67,7 @@ class AutoTitle : Collector
             await File.WriteAllTextAsync("./customTitle.json", finalJson);
         }
 
-        Console.WriteLine("update successful");
+        watch.Stop();
+        Console.WriteLine($"update successful ☑ {watch.Elapsed}");
     }
 }
