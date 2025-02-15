2d3a492505bb4b412d67376e56e1c19bf106185a
sobel <nppstcc1001050311@gmail.com>
2025-02-15 12:30:37 +0800

    make AsParallel

--- LocalDurationDiff.cs
+++ LocalDurationDiff.cs
@@ -1,15 +1,20 @@
+using System.Diagnostics;
 using System.Text;
 using FFMpegCore;
 using YTPlayListDownloader.Collectors;
 
 class LocalDurationDiff : Collector
 {
+    private Stopwatch watch;
     public LocalDurationDiff(string url) : base(url)
     {
+        watch = new Stopwatch();
     }
 
     public override async Task Invoke()
     {
+        watch.Start();
+
         var playListInfo = await GetPlayListInfo(url);
 
         var playList = await yt.Playlists.GetVideosAsync("https://www.youtube.com/playlist?list=PLdx_s59BrvfXJXyoU5BHpUkZGmZL0g3Ip")
@@ -23,12 +28,12 @@ class LocalDurationDiff : Collector
 
         string path = $"D:\\source-codes\\ytPlaylistDownloader\\YT-{playListInfo.title}";
 
-        var mp3_files = Directory.GetFiles(path)
-                                 .Select( mp3Path =>
+        var mp3_files = Directory.GetFiles(path).AsParallel().WithDegreeOfParallelism(4)
+                                 .Select(mp3Path =>
                                  {
                                      var mp3 = TagLib.File.Create(mp3Path);
 
-                                     var mediaInfo =  FFProbe.Analyse(mp3Path);
+                                     var mediaInfo = FFProbe.Analyse(mp3Path);
 
                                      return (mp3.Tag.Track, mp3.Tag.Title, mp3.Properties.Duration);
                                  })
@@ -37,19 +42,21 @@ class LocalDurationDiff : Collector
 
         using (var report = File.Create("./LengthDiffReport.csv"))
         {
-            
+
             for (int i = 0; i < playList.Count; i++)
             {
                 var p = await playList[i];
                 var m = mp3_files.Where(item => item.Track == i + 1).FirstOrDefault();
 
-                var message=$"{i+1:000}, {p.Title}, Youtube與檔案長度差異:\t{p.Duration - m.Duration}\n";
+                var message = $"{i + 1:000}, {p.Title}, Youtube與檔案長度差異:\t{p.Duration - m.Duration}\n";
                 byte[] info = new UTF8Encoding(true).GetBytes(message);
                 Console.WriteLine(message);
                 report.Write(info, 0, info.Length);
             }
         }
 
-        Console.WriteLine("ok!");
+        watch.Stop();
+
+        Console.WriteLine($"☑ {watch.Elapsed}");
     }
 }
